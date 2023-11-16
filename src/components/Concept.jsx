04167216db29f8b5dbd7f1a1a61a9e74c214a6ee
bandeau <augusta.bande@ext.ec.europa.eu>
2023-11-16 20:01:06 +0100

    feat: added Concept

--- src/components/Concept.jsx
+++ src/components/Concept.jsx
@@ -1,33 +1,34 @@
 import React from "react";
 const Concept = () => {
   return (
-    <section id="concept" className="section bg-light">
+    <section id="concept" className="section bg-primary">
       <div className="container">
         <div className="row gy-5">
           <div className="col-lg-6 order-1 order-lg-0 wow fadeInUp">
             {/* Heading */}
             <p className="text-center text-lg-start mb-2">
-              <span className="bg-primary text-light px-2">Concept</span>
+              <span className="bg-light px-2">Concept</span>
             </p>
-            <h2 className="text-10 fw-600 text-center text-lg-start mb-5">
-              Coding with creativity: my philosophy
+            <h2 className="text-10 fw-600 text-center text-white text-lg-start mb-5">
+              Coding with creativity: <br></br>my philosophy
             </h2>
+            <p className=" text-left text-white mb-2 wow fadeInUp">At the heart of digital innovation, MANA'O IT SOLUTION embodies the spirit of the Hawaiian word "mana'o", meaning deep knowledge and insight. As a dedicated web developer, I am committed to transforming this wisdom into tangible digital solutions. </p>
             {/* Heading end*/}
 
             
           </div>
           <div
-            className="col-lg-6 d-flex align-items-center justify-content-center order-0 order-lg-1 wow fadeIn"
+            className="col-lg-6 order-1 order-lg-0 wow fadeInUp"
             data-wow-delay="1s"
           >
+            
+            
+            <p className=" text-left text-white mb-2 wow fadeInUp"> I blend creativity, functionality, and cutting-edge technology to exceed my clients' needs. Every web project is a unique journey. My goal is to infuse "mana'o" - profound understanding - into every website, application, or platform I develop. </p><br></br>
+            <p className=" text-left text-white mb-2 wow fadeInUp">Whether you're launching a new website, revamping an existing digital presence, or exploring innovative solutions, MANA'O IT SOLUTION is here to guide you with expertise and creativity. Join me on this journey of digital transformation, where your vision meets my expertise to create something truly extraordinary.</p>
+
           </div>
         </div>
-        <div className="row gy-5">
-            <p className=" text-center mb-2 wow fadeInUp">At the heart of digital innovation, MANA'O IT SOLUTION embodies the spirit of the Hawaiian word "mana'o", signifying deep knowledge and insight. As a dedicated web developer, I am committed to transforming this wisdom into tangible digital solutions. I blend creativity, functionality, and cutting-edge technology to exceed my clients' needs.</p>
-            <p className=" text-center mb-2 wow fadeInUp">MANA'O IT SOLUTION believes every web project is a unique journey. My goal is to infuse "mana'o" - profound understanding - into every website, application, or platform I develop. I ensure each project not only achieves its objectives but surpasses them, offering an exceptional user experience, intuitive design, and top-tier performance.</p>
-            <p className=" text-center mb-2 wow fadeInUp">Whether you're launching a new website, revamping an existing digital presence, or exploring innovative solutions, MANA'O IT SOLUTION is here to guide you with expertise and creativity. Join me on this journey of digital transformation, where your vision meets my expertise to create something truly extraordinary.</p>
-
-        </div>
+       
       </div>
       
       
